b78519fce3761fdd5dd139efc17d82a41215c871
rihlaula <rihlarahmat20@gmail.com>
2023-12-12 11:53:40 +0700

    praktikum11

---
+++ Pemweb_Praktikum11/parkir.js
@@ -0,0 +1,22 @@
+function hitungParkir(){
+    // Tangkap angka yang user input
+    let masuk = document.getElementById('masuk').value
+    let keluar = document.getElementById('keluar').value
+
+    // Tangkap span untuk hasil
+    let hasilDurasi = document.getElementById("hasilDurasi")
+    let totalBiaya = document.getElementById("totalBiaya")
+
+    // Hitung durasi parkir
+    let durasi = keluar - masuk
+
+    // Hitung biaya parkir
+    let biaya = 3000
+    for(let i = 2; i < durasi; i++){
+        biaya = biaya + 1000
+    }
+
+    // menampilkan hasil perhitungan
+    hasilDurasi.innerHTML = durasi
+    totalBiaya.innerHTML = biaya
+}
