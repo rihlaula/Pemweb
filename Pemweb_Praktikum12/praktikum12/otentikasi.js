f4d62850e1f979da37438d8ad6d716cf1efeff70
rihlaula <rihlarahmat20@gmail.com>
2023-12-26 22:38:17 +0700

    push

---
+++ Pemweb_Praktikum12/praktikum12/otentikasi.js
@@ -0,0 +1,18 @@
+const username = "Rihla"
+const password = "12345678"
+
+function auth() {
+    // tangkap nilai yang di input user
+    let userInput = document.getElementById("username").value
+    let passwordInput = document.getElementById("password").value
+
+    let form = document.getElementById('form')
+
+    if(username == userInput && password == passwordInput){
+        alert("Login Berhasil")
+        form.submit()
+    } else {
+        alert("Login Gagal!")
+    }
+}
+
